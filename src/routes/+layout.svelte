59e33ca6af2fcbd2c09cf002e1e86b7594d6e40f
Baby Feavel <feavel@qq.com>
2025-07-24 23:20:36 +0800

    Sure! Pl

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.css';
+	import { page } from '$app/state';
+	import { locales, localizeHref } from '$lib/paraglide/runtime';
+
+	let { children } = $props();
+</script>
+
+<div style="display:none">
+	{#each locales as locale}
+		<a href={localizeHref(page.url.pathname, { locale })}>{locale}</a>
+	{/each}
+</div>
+
+{@render children()}
